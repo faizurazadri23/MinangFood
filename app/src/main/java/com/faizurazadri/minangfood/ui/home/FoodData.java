4c045075110c7b4692eefa9bbc545f5df74ac201
faizurazadri23 <faizurazadri08@gmail.com>
2021-09-14 08:56:39 +0700

    Pergantian data

--- app/src/main/java/com/faizurazadri/minangfood/ui/home/FoodData.java
+++ app/src/main/java/com/faizurazadri/minangfood/ui/home/FoodData.java
@@ -1,7 +1,6 @@
 package com.faizurazadri.minangfood.ui.home;
 
 
-import com.faizurazadri.minangfood.R;
 import com.faizurazadri.minangfood.model.Food;
 
 import java.util.ArrayList;
@@ -9,151 +8,48 @@ import java.util.ArrayList;
 public class FoodData {
 
     private static String[] foodName = {
-            "Tropicana Slim Cafe Latte (10 Sachet) - Sugar FREE",
-            "Paket Movie Fun (1BOB, 1FM, 1TBC) - Jolly Time Mircowave Popcorn",
-            "Gery Kreker Beras - 5,6g (1 Bag Isi 10 Pcs) - BRP10",
-            "Tropicana Slim Oat Drink 190ml (4 pcs) - Ready to Drink",
-            "PAKET ISI 2 - Mujigae Topokki 300gr / Teokbokki Instan / Tokpoki",
-            "Dilan Choco Crunchy Caramel Pouch",
-            "Beras Raja Platinum 5 Kg",
-            "Sedaap Mie Instan Singapore Spicy Laksa 83 Gr",
-            "PROCHIZ Trial Package (Cheddar, Gold, Spready, Quick Melt)",
-            "Garuda Rosta Kacang Oven Rasa Bawang x 3 pcs"
+            "Soles Padang",
+            "Rakik Maco",
+            "Bumbu Apik Lauak",
+            "Ladu",
+            "Gulai Baga",
+            "Gulai Baga"
     };
 
     public static String[] food_Description = {
-            "Tropicana Slim Cafe Latte terbuat dari biji kopi pilihan yang dikombinasikan dengan gula rendah kalori dan krimer yang pastinya membuat minum kopi jadi suatu ritual nikmat tanpa waswas. Tidak percaya? Segera seduh dan nikmati secangkir Cafe Latte ala Tropicana Slim. Sugar free, less calorie, for your sinful indulgence",
-            "Paket Movie Fun :\n" +
+            "Soles dengan isian suwiran daging ayam nan gurih bercampur sayuran seperti wortel. Ditambah pedasnya karakter padang dan dibalut krimis di luar.",
+            "Rakik maco sejenis makanan ringan yang pada dasarnya keripik, tapi ditambah ikan maco sebagai tambahan gizi dan membuatnya lebih nikmat. Dapat dinikmati langsung atau sebagai pendamping nasi.",
+            "\n" +
                     "\n" +
-                    "1 Porsi (serving) Microwave Popcorn mentega terbaik kami dengan gaya popcorn bioskop dengan rasa ekstra mentega,\n" +
-                    "1 Porsi Microwave Popcorn Gourmet rasa manis dan asin seperti di karnival-karnival dan\n" +
-                    "1 porsi Microwave Popcorn rasa keju cheddar terbaik dalam 1 paket.",
-            "    Sertifikat: Halal\n" +
-                    "    Kondisi: Baru\n" +
-                    "    Berat: 70 Gram\n" +
-                    "    Kategori: Biskuit & Wafer\n" +
-                    "    Etalase: Gery\n" +
+                    "Apik lauak artinya ikan yang dibakar dengan pemanggang tradisional minangkabau, sedangkan bumbunya adalah bumbu khas pariaman, makanan khas pesisir yang melegenda juga dengan sala lauaknya.\n" +
                     "\n" +
-                    "Gery Kreker Beras - 5,6g (1 Bag Isi 10 Pcs) - BRP10",
-            "Tropicana Slim Oat Drink adalah minuman oat bebas gula, rendah lemak, dan merupakan sumber serat dan kalsium dengan rasa vanilla yang nikmat sehingga cocok untuk diet dan aman untuk diabetesi.\n" +
+                    "Bumbu ini kami siapkan untuk 1 KG ikan dengan ukuran sedang (sepanjang 10-15 cm). Bumbu ini tidak pedas, jadi bisa di konsumsi oleh anak-anak atau yang tidak penyuka pedas.\n" +
                     "\n" +
-                    "Mengapa Tropicana Slim Oat Drink?\n" +
-                    "- Bebas gula\n" +
-                    "- Rendah lemak\n" +
-                    "- Sumber serat dan kalsium\n" +
-                    "- Rasa vanilla yang nikmat\n" +
-                    "- Cocok untuk diet dan aman untuk diabetesi\n" +
-                    "- Praktis dibawa kemana saja\n" +
+                    "Selain bumbu ini anda sebaiknya menambahkan perasan santan kental untuk di taburi di atas ikan saat pembakaran, menambah aroma sangat harum\n" +
                     "\n" +
-                    "Cara Penyajian dan Petunjuk Konsumsi :\n" +
-                    "Kocok dahulu sebelum dikonsumsi. Disajikan dingin lebih nikmat.",
-            "Topokki by Mujigae ( Tteokbokki )\n" +
-                    "Terdiri dari: 120 gram Topokki dan 180 gram Saus, Fish Cake, Dobu\n" +
-                    "\n" +
-                    "Halal, Tanpa Bahan Pengawet, dan Dapat Dikirim ke Seluruh indonesia (Dapat disimpan hingga 6 Bulan).\n" +
-                    "\n" +
-                    "Bagi kalian pecinta masakan korea pasti sudah tidak asing lagi dengan hidangan khas korea yaitu Topokki. Tteokbokki atau Topokki merupakan salah satu hidangan kaki lima yang populer asal korea.\n" +
-                    "\n" +
-                    "Kini tidak perlu repot lagi jika ingin menikmati Topokki langsung di rumah karena sudah tersedia Topokki by Mujigae. Produk ini merupakan Alternatif terbaik karena menggunakan bahan baku pilihan dan di proses menggunakan teknologi tinggi berupa proses sterilisasi yang kemudian dikemas dengan cara di vacuum untuk menghasilkan kualitas yang Optimal dan tahan lama sehingga aman untuk dikonsumsi. Topokki by Mujigae memiliki perpaduan rasa yang tepat antara rice cake dan saus gochujang menghasilkan cita rasa yang unik khas Korea. Topokki by Mujigae Halal dan dijamin Fresh!!!\n" +
-                    "\n" +
-                    "SERVING SUGESTION\n" +
-                    "a. Buka Kemasan Topokki\n" +
-                    "b. Pisah-pisahkan topokki\n" +
-                    "c. Siapkan wajan\n" +
-                    "d. Masukan topokki ke dalam wajan\n" +
-                    "e. Buka kemasan saus, kemudian tuang diatas topokki\n" +
-                    "f. Pastikan topokki terendam oleh saus\n" +
-                    "g. Nyalakan kompor dalam api sedang\n" +
-                    "h. Masak topokki selama 4-5 menit sampai mendidih\n" +
-                    "i. Setelah matang angkat dan sajikan selagi hangat\n" +
-                    "\n" +
-                    "Komposisi :\n" +
-                    "Tteok: Beras, Garam\n" +
-                    "Saus: Gochujang, saus tomat, air, gula, garam, beef dasida\n" +
-                    "Topping: Dobu, Fish Cake\n" +
-                    "\n" +
-                    "Mujigae, Restaurant Korea Pertama di Indonesia yang mendapatkan sertifikasi halal MUI pada tahun 2017, Pemegang hak tunggal adalah PT.Animo Resto Primera yang didirikan oleh kelompok usaha Passionfood pada tahun 2013, Restaurant Mujigae pertama dibuka pada bulan April tahun 2013 yang berlokasi di Cihampelas walk Bandung. Saat ini Mujigae memiliki 19 Outlet yang tersebar di Jabodetabek – Cikarang – Karawang dan Bandung.\n" +
-                    "\n" +
-                    "Sertifikat MUI: 03160016450417\n" +
-                    "PIRT: 2063202011188-20",
-            "Kondisi: Baru\n" +
-                    "    Berat: 200 Gram\n" +
-                    "    Kategori: Cokelat\n" +
-                    "    Etalase: NEW PRODUCT GARUDAFOOD\n" +
-                    "\n" +
-                    "DILAN Choco Crunchy C.Pouch -9.5g x 10pcs (3 Pouch)",
-            "Kondisi: Baru\n" +
-                    "    Berat: 2.000 Gram\n" +
-                    "    Kategori: Beras Putih\n" +
-                    "    Etalase: Sembako\n" +
-                    "\n" +
-                    "Beras Raja Platinum 5 Kg\n" +
-                    "\n" +
-                    "Beras putih yang punya tekstur pulen seperti beras pada umumnya. Butiran beras\n" +
-                    "bagus dan warnanya putih bersih.\n" +
-                    "\n" +
-                    "- Berat 5 Kg",
-            "Kondisi: Baru\n" +
-                    "    Berat: 100 Gram\n" +
-                    "    Kategori: Mie Instan\n" +
-                    "    Etalase: Sedaap Laksa\n" +
-                    "\n" +
-                    "Mie Sedaap Selection Singapore Spicy Laksa BARU!!\n" +
-                    "Paduan mi kenyal dengan kuah laksa kaya rasa yang gurih, ditambah level pedasnya bisa diatur sendiri, dijamin bikin nagih! The taste that can brings you back to Singapore!\n" +
-                    "Laksa Singapore beneran, kuah gurihnya mantap, laksanya beneran Sedaap!\n" +
-                    "\n" +
-                    "Cara Penyajian:\n" +
-                    "- Rebus mi hingga matang kurang lebih 3 menit\n" +
-                    "- Siapkan bumbu bubuk dan krimer ke mangkok\n" +
-                    "- Tuang mi beserta kuah ke mangkok\n" +
-                    "- Taburkan bubuk cabe sesuai selera\n" +
-                    "\n" +
-                    "Keunggulan:\n" +
-                    "- Tekstur Mie Kenyal\n" +
-                    "- Kuah Gurih dan Creamy",
-            "Kondisi: Baru\n" +
-                    "    Berat: 700 Gram\n" +
-                    "    Kategori: Keju\n" +
-                    "    Etalase: Semua Etalase\n" +
-                    "\n" +
-                    "PROCHIZ Trial Package terdiri dari:\n" +
-                    "1. Cheddar 170gr (1pc)\n" +
-                    "2. Gold 170gr (1pc)\n" +
-                    "3. Spready 170gr (1pc)\n" +
-                    "4. Quick Melt 170gr (1pc)",
-            "Sertifikat: Halal\n" +
-                    "    Kondisi: Baru\n" +
-                    "    Berat: 300 Gram\n" +
-                    "    Kategori: Kacang\n" +
-                    "    Etalase: Kacang Garuda\n" +
-                    "\n" +
-                    "Isi 3 Pcs Garuda Rosta Kacang Oven Rs Bawang -100g (ORBM) 3 Pcs"
+                    "Bumbu baru kami buatkan saat akan dikirim, jadi bukan frozen food\n",
+            "Ladu adalah makanan khas minang, panganan untuk bersantai ini dapat anda pesan dan bisa tahan lebih dari 5 hari setelah dibuka dari packaging",
+            "Gulai baga adalah gulai tanpa santa khas pariaman, menggunakan daging yang bercampur lemak, sejenis sup tapi pedas. Sehat dan fresh. Kami tidak mengirimkan untuk perjalanan lebih dari 20 jam",
+            "Gulai baga adalah gulai tanpa santa khas pariaman, menggunakan daging yang bercampur lemak, sejenis sup tapi pedas. Sehat dan fresh. Kami tidak mengirimkan untuk perjalanan lebih dari 20 jam",
     };
 
     public static String[] food_address = {
-            "Tokopedia",
-            "Jakarta Barat",
-            "Tokopedia",
-            "Tokopedia",
-            "Jakarta Timur",
-            "Tokopedia",
-            "Kota Tangerang",
-            "Tokopedia",
-            "Tokopedia",
-            "Tokopedia"
+            "Padang",
+            "Solok",
+            "Sawah Lunto",
+            "Pesisir Selatan",
+            "Bukit Tinggi",
+            "Cupak"
+
     };
 
     public static String[] food_image = {
-            "https://images.tokopedia.net/img/cache/900/VqbcmM/2021/4/15/422bd8a2-ddda-4699-8539-b98d67be6e92.jpg",
-            "https://images.tokopedia.net/img/cache/700/VqbcmM/2021/3/29/9e1ca31d-9540-4ca3-889a-680111155fc3.jpg",
-            "https://images.tokopedia.net/img/cache/700/VqbcmM/2021/2/10/0d9e0319-50df-4b1f-a2eb-129737ffe75d.jpg",
-            "https://images.tokopedia.net/img/cache/700/VqbcmM/2021/7/12/194e2975-c83a-4c40-81a2-e5205840d8e4.jpg",
-            "https://images.tokopedia.net/img/cache/700/product-1/2020/7/8/45361738/45361738_9c8d8f08-61ce-422a-a90b-28fc8f26495c_500_500",
-            "https://images.tokopedia.net/img/cache/900/VqbcmM/2021/6/15/8afa7f95-4948-4b11-ba0d-97046613c14c.jpg",
-            "https://images.tokopedia.net/img/cache/900/VqbcmM/2021/9/7/83bfdacf-943e-4e29-a3a1-e5f0e2b592d0.jpg",
-            "https://images.tokopedia.net/img/cache/900/VqbcmM/2021/5/24/6c2c9c2b-d1b1-44d7-bf37-99731eb92077.jpg",
-            "https://images.tokopedia.net/img/cache/900/VqbcmM/2021/9/3/66beb001-14a3-4a40-8627-51c7edc35836.jpg",
-            "https://images.tokopedia.net/img/cache/900/VqbcmM/2021/9/9/92f2daee-1653-46fa-9668-b1b494e4f894.jpg",
+            "https://demo.idwebdata.com/toko/wp-content/uploads/2020/05/soles-100x100.jpg",
+            "https://demo.idwebdata.com/toko/wp-content/uploads/2020/05/11036949_1583576418554879_5625467794976026703_n-100x100.jpg",
+            "https://demo.idwebdata.com/toko/wp-content/uploads/2020/05/IMG_1603-scaled-100x100.jpg",
+            "https://demo.idwebdata.com/toko/wp-content/uploads/2020/05/11393019_1616688045243716_2270826192999947824_o-100x100.jpg",
+            "https://demo.idwebdata.com/toko/wp-content/uploads/2020/05/10846446_1543463522566169_3754704038031399875_n-100x100.jpg",
+            "https://demo.idwebdata.com/toko/wp-content/uploads/2020/05/10846446_1543463522566169_3754704038031399875_n-100x100.jpg"
     };
 
     public static String[] food_rate = {
@@ -162,24 +58,16 @@ public class FoodData {
             "4.9",
             "4.9",
             "4.9",
-            "4.9",
-            "5.0",
-            "4.9",
-            "5.0",
             "4.9"
     };
 
     public static String[] food_Detail = {
-            "https://www.tokopedia.com/nutrimartid/tropicana-slim-cafe-latte-10-sachet-sugar-free",
-            "https://www.tokopedia.com/jollytimeoc/paket-movie-fun-1bob-1fm-1tbc-jolly-time-mircowave-popcorn",
-            "https://www.tokopedia.com/garudafood/gery-kreker-beras-5-6g-1-bag-isi-10-pcs-brp10",
-            "https://www.tokopedia.com/nutrimartid/tropicana-slim-oat-drink-190ml-4-pcs-ready-to-drink",
-            "https://www.tokopedia.com/mujigaeofficial/paket-isi-2-mujigae-topokki-300gr-teokbokki-instan-tokpoki",
-            "https://www.tokopedia.com/garudafood/dilan-choco-crunchy-caramel-pouch",
-            "https://www.tokopedia.com/kedaimartstore/beras-raja-platinum-5-kg",
-            "https://www.tokopedia.com/wingsofficial/sedaap-mie-instan-singapore-spicy-laksa-83-gr",
-            "https://www.tokopedia.com/prochiz/prochiz-trial-package-cheddar-gold-spready-quick-melt",
-            "https://www.tokopedia.com/garudafood/garuda-rosta-kacang-oven-rasa-bawang-x-3-pcs"
+            "https://demo.idwebdata.com/toko/product/soles-padang/",
+            "https://demo.idwebdata.com/toko/product/rakik-maco/",
+            "https://demo.idwebdata.com/toko/product/bumbu-apik-lauak/",
+            "https://demo.idwebdata.com/toko/product/ladu/",
+            "https://demo.idwebdata.com/toko/product/gulai-baga/",
+            "https://demo.idwebdata.com/toko/product/gulai-baga/"
     };
 
     static ArrayList<Food> getListData() {
